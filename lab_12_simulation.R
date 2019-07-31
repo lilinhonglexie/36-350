b47538430bfae6b0423ad46e69860643a702ed70
Linhong Li <linhongl@Linhongs-MBP.wv.cc.cmu.edu>
2019-07-31 13:24:49 -0400

    Added model selection.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,3 +8,17 @@ generate_data <- function(n, p){
               responses = responses))
 }
 
+model_select <- function(covariates, responses, cutoff){
+  full_model <- lm(responses ~ covariates)
+  full_model_ps <- 
+    summary(full_model)$coefficients[, "Pr(>|t|)"][-1] # ignore the intercept p
+  significant_covars <- full_model_ps <= cutoff
+  if (sum(significant_covars) == 0){
+    return(vector())   # return empty vector
+  } else {
+    reduced_model <- lm(responses ~ covariates[, significant_covars])
+    reduced_model_ps <-
+      summary(reduced_model)$coefficients[, "Pr(>|t|)"][-1] # ignore the intercept p
+    return(reduced_model_ps)
+  }
+}
